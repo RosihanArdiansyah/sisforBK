71300eec5ba71cb545d68f41ac816ff107d3cf82
Rosihan Ardiansyah <rosihan17@gmail.com>
2023-11-11 10:37:42 +0700

    edit user_2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -9,4 +9,10 @@ class UserModel extends Model
     protected $table = 'user';
     protected $primaryKey = 'ID';
     protected $allowedFields = ['username', 'fullName', 'NIS', 'TTL', 'Bapak', 'Ibu', 'Kelas', 'Role', 'Password'];
+
+    public function editUser($data, $id)
+    {
+        $query = $this->db->table($this->table)->update($data, array('id' => $id));
+        return $query;
+    }
 }
